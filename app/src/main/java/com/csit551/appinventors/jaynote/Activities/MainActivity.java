e3384e6b13d8df9eb27cbe3761f14d1327d03d02
Alicina Memar <memaralicina@gmail.com>
2015-04-22 19:36:14 -0400

    Trying to debug actionbar icon issues. Added different resolutions of logo/launcher.

--- app/src/main/java/com/csit551/appinventors/jaynote/Activities/MainActivity.java
+++ app/src/main/java/com/csit551/appinventors/jaynote/Activities/MainActivity.java
@@ -31,7 +31,6 @@ public class MainActivity extends ActionBarActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         db = new DatabaseManager(MainActivity.this.getApplicationContext());
         context = this.getBaseContext();
 
